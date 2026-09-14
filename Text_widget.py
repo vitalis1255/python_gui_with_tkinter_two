8806268e9c29e8df3df2d4c164abf5d7d60878d9
vitalis1255 <vitalis.onyeke.175508@unn.edu.ng>
2026-09-14 12:41:38 +0100

    make change

---
+++ Text_widget.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+root = Tk()
+root.title("Text Widget Example")
+root.geometry("400x300+200+50")
+
+scroll = Scrollbar(root)
+scroll.pack(side=RIGHT,fill=Y)
+
+def fetch():
+  content = text.get("1.0",END)
+  label = Label(root, text="User Input: " + content)
+  label.pack(pady=10)
+
+text = Text(root,
+            yscrollcommand=scroll.set,
+            height=10,
+            width=40,
+            font=("Arial",12),
+            fg="blue",bg="yellow",
+            wrap="word",relief="groove",
+            borderwidth=2)
+text.pack(pady=20)
+scroll.config(command=text.yview)
+
+
+btn = Button(root,text="Fetch Text",command=fetch)
+btn.pack(pady=10)
+root.mainloop()
